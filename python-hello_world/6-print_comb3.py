a9190a38a4eb6d3bcc014e33651766d2da846fae
Afkkaoutar <136103469+Afkkaoutar@users.noreply.github.com>
2023-07-19 01:24:12 +0100

    done for today

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
-for num1 in range(10):
-    for num2 in range(num1 + 1, 10):
-        print(f"{num1}{num2}".format(num1,num2), end=", ")
-print()
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end="")
