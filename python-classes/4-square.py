6a40ae42ba0f899b211d2d9aa08ca3b36aae095e
Afkkaoutar <136103469+Afkkaoutar@users.noreply.github.com>
2023-09-23 02:31:16 +0100

    nb4

---
+++ python-classes/4-square.py
@@ -0,0 +1,37 @@
+"""
+Printing a square
+"""
+class Square():
+
+    def __init__(self, size=0):
+       
+        self.__size = size
+
+    @property
+    def size(self):
+      
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+       
+        if value != int(value):
+            raise TypeError("size must be an integer")
+        if value  < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        
+        sq_area = self.__size ** 2
+        return sq_area
+    
+    def my_print(self):
+        
+        if self.__size == 0:
+            print()
+
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
