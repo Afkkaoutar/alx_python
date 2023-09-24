1fc6628e8f4e45c1c4766f4479b0dbb8629e1ba3
Afkkaoutar <136103469+Afkkaoutar@users.noreply.github.com>
2023-09-24 02:20:06 +0100

    00

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,10 +1,14 @@
+# Import the class Flask from flask module
 from flask import Flask
 
+# Create an instance of Flask class
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
+
     return "Hello HBNB!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
